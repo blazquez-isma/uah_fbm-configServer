3a00d48a8e04b85854eab3c5e2b84fe0210dd529
Ismael Blazquez Carabias <ismael.blazquez@edu.uah.es>
2025-02-22 17:50:02 +0100

    Creacion del proyecto

---
+++ src/test/java/es/uah/ismael/fbm/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.ismael.fbm.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
